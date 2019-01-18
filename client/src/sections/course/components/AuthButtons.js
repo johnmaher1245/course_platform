3122fc753c59c8ebf33271f66ed446eeeff99d54
John Maher <john@Johns-iMac.local>
2019-01-18 09:54:41 -0500

    fixing auth button styles

--- client/src/sections/course/components/AuthButtons.js
+++ client/src/sections/course/components/AuthButtons.js
@@ -4,11 +4,11 @@ import googleIcon from '../../../assets/icons/google.png'
 const AuthButton = props => {
   const { google } = props;
   return google ? (
-    <a href="/auth/google" className="relative google-button flex row align-center" >
+    <a href="/auth/google" className="relative google-button flex row align-center" style={{justifyContent: 'center'}} >
      <img style={{left: '16px'}} className='absolute' src={googleIcon} alt="google logo"></img><span>Login with Google</span>
     </a>
   ) : (
-    <a  href="/auth/facebook" className="relative facebook-button flex row align-center" >
+    <a  href="/auth/facebook" className="relative facebook-button flex row align-center" style={{justifyContent: 'center'}}
       <img style={{left: '16px'}} className='absolute' src={facebookIcon} alt="facebook logo"></img><span>Login with Facebook</span>
     </a>
   );
